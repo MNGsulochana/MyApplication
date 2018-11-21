cbd88dce5fe7819601bdfd1240b264e5e4cefd52
MNGsulochana <masulochana@gmail.com>
2018-11-21 19:12:52 +0530

    second

--- app/src/main/java/com/example/sulochana/myapplication/sample/ActivityBasicSample.java
+++ app/src/main/java/com/example/sulochana/myapplication/sample/ActivityBasicSample.java
@@ -3,6 +3,9 @@ package com.example.sulochana.myapplication.sample;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.sulochana.myapplication.R;
 
@@ -11,6 +14,19 @@ public class ActivityBasicSample extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_basic_sample);
+        setContentView(R.layout.custom_layout);
+
+
+        TagLayout tagLayout = (TagLayout) findViewById(R.id.customlayout);
+        LayoutInflater layoutInflater = getLayoutInflater();
+        /*String tag;
+        for (int i = 0; i <= 20; i++) {
+            tag = "#tag" + i;
+            View tagView = layoutInflater.inflate(R.layout.tag_layout, null, false);
+
+            TextView tagTextView = (TextView) tagView.findViewById(R.id.tagTextView);
+            tagTextView.setText(tag);
+            tagLayout.addView(tagView);
+        }*/
     }
 }
